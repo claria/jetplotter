474f5f252f745acde8ed230863c0853aa6c0580a
Georg Sieber <g.sieber@gmail.com>
2015-08-12 18:18:01 +0200

    doc and more items for lookup dict.

--- src/lookup_dict.py
+++ src/lookup_dict.py
@@ -1,6 +1,13 @@
-
+"""
+Dictionary containing lookup values which will be replaced in the 
+settings.
+"""
 lookup_dict = {
         'x_label' : {
-            '_ptavg_' : '$p_{\mathrm{T,avg}}'
+            '_ptavg_' : '$p_{\mathrm{T,avg}}$ (GeV)'
+            },
+        'y_label' : {
+            '_ptavg_' : '$p_{\mathrm{T,avg}}$ (GeV)'
             }
+
         }
