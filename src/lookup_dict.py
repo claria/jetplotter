0aa98e2b304855a703f58f7e34b0208398c36566
Georg Sieber <g.sieber@gmail.com>
2015-08-13 17:13:56 +0200

    shortcuts for colors

--- src/lookup_dict.py
+++ src/lookup_dict.py
@@ -4,10 +4,20 @@ settings.
 """
 lookup_dict = {
         'x_label' : {
-            '_ptavg_' : '$p_{\mathrm{T,avg}}$ (GeV)'
+            '_ptavg_' : '$p_{\mathrm{T,avg}}$ (GeV)',
+            '_y_boost_' : '$y_b$'
             },
         'y_label' : {
-            '_ptavg_' : '$p_{\mathrm{T,avg}}$ (GeV)'
+            '_ptavg_' : '$p_{\mathrm{T,avg}}$ (GeV)',
+            '_y_star_' : '$y_s$'
+            },
+        'color' : {
+            '_color0_' : '#4C72B0',
+            '_color1_' : '#55A868',
+            '_color2_' : '#C44E52',
+            '_color3_' : '#8172B2',
+            '_color4_' : '#CCB974',
+            '_color5_' : '#64B5CD'
             }
 
         }
