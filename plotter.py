a136c70f6a9a2d47f3ea47d63d9ddee6c763e16f
Georg Sieber <g.sieber@gmail.com>
2015-08-15 13:42:57 +0200

    add logger

--- plotter.py
+++ plotter.py
@@ -2,8 +2,11 @@
 
 from src.core import Plotter
 
+import logging
+log = logging.getlogger(__name__)
 
 def main():
+    """Initializes and runs the core."""
     plotter = Plotter()
     plotter()
 
