5e74985e04d147ede09e3b0d7fe7b7e0aa133c43
Georg Sieber <g.sieber@gmail.com>
2015-08-16 18:57:22 +0200

    update jsons

--- plotter.py
+++ plotter.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
-
+import os
+import sys
+# sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 from src.core import Plotter
 
 import logging
-log = logging.getlogger(__name__)
+log = logging.getLogger(__name__)
 
 def main():
     """Initializes and runs the core."""
