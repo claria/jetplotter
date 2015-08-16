e6813ab93b79b8ad5fece9f578a0aff81ee8a14f
Georg Sieber <g.sieber@gmail.com>
2015-08-16 19:09:38 +0200

    reformatted code, fixex syntactical things.

--- plotter.py
+++ plotter.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
-import os
-import sys
+# import os
+# import sys
 # sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+import logging
+
 from src.core import Plotter
 
-import logging
 log = logging.getLogger(__name__)
+
 
 def main():
     """Initializes and runs the core."""
     plotter = Plotter()
     plotter()
+
 
 if __name__ == '__main__':
     main()
