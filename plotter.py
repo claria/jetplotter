0d21a2f3b1d937cf9a69809a5a66477dc571dd26
Georg Sieber <g.sieber@gmail.com>
2015-08-17 13:57:48 +0200

    add sys path

--- plotter.py
+++ plotter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-# import os
-# import sys
-# sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
+import os
+import sys
+sys.path.append(os.path.dirname(__file__))
 import logging
 
 from src.core import Plotter
