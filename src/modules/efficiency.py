35af1e1092dac23938d6358c10b562c222ccb26b
Georg Sieber <g.sieber@gmail.com>
2015-08-18 22:49:24 +0200

    add efficiency calculation tool

---
+++ src/modules/efficiency.py
@@ -0,0 +1,38 @@
+import logging
+
+import ROOT
+from src.modules.base_module import BaseModule
+
+log = logging.getLogger(__name__)
+
+
+class Efficiency(BaseModule):
+    def __init__(self):
+        super(Efficiency, self).__init__()
+        print 'hallo'
+        self.parser.add_argument('--efficiency', nargs='+', type='str2kvstr', help='')
+
+    def __call__(self, config):
+        print config.keys()
+        for id, val in config['efficiency']:
+            if id not in config['objects']:
+                raise ValueError('Requested id {} not found.'.format(id))
+            if val not in config['objects']:
+                raise ValueError('Requested id {} not found.'.format(val))
+
+            numerator = config['objects'][id]['obj']
+            denominator = config['objects'][val]['obj']
+            eff_graph = ROOT.TGraphAsymmErrors(numerator, denominator)
+
+            new_id = 'eff_{0}'.format(id.strip('_'))
+            config['objects'].setdefault(new_id, {})
+            config['objects'][new_id]['obj'] = eff_graph
+
+
+def isfloat(value):
+    """Return true if value can be converted to float."""
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
