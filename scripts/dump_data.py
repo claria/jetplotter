1ca961032a9208fcdf6a75bd85dd3074fe971f4a
Georg Sieber <g.sieber@gmail.com>
2015-09-02 13:07:45 +0200

    template to dumpt all neccessary data for herafitter.

---
+++ scripts/dump_data.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+from ..util import root2mpl
+
+
+def main():
+    pass
+
+    
+
+# unfolded data
+# stat unc
+# correlations
+# jec
+# systematic unfolding
+
+
+
+
+
+if __name__ == '__main__':
+    main()
