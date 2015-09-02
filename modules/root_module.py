32bb51ef48c35e219b02265ec12ab732ebb224c5
Georg Sieber <g.sieber@gmail.com>
2015-09-02 10:58:58 +0200

    add module to build tgraphs. fix output module.

--- modules/root_module.py
+++ modules/root_module.py
@@ -8,6 +8,9 @@ ROOT.gROOT.SetBatch(True)
 from modules.base_module import BaseModule
 
 
+import logging
+log = logging.getLogger('__name__')
+
 class RootModule(BaseModule):
     """ Input module to load histograms and graphs from a root file.
 
@@ -42,11 +45,11 @@ class RootModule(BaseModule):
 class RootOutputModule(BaseModule):
     def __init__(self):
         super(RootOutputModule, self).__init__()
-        self.arg_group.add_argument('--root-output', default=None, nargs='+', type='str',
+        self.arg_group.add_argument('--root-output', default=None, nargs='+', type=str,
                                     help='List of ids, which objects will be written out to a root file.')
 
     def __call__(self, config):
-        if config['root-output']:
+        if config.get('root-output', []):
             ids = config['root-output']
         else:
             ids = config['objects'].keys()
@@ -60,9 +63,83 @@ class RootOutputModule(BaseModule):
         for id in ids:
             if id not in config['objects']:
                 raise ValueError('Id {0} not found within objects. Check your supplied ids.'.format(id))
-            config['objects'][id]['obj'].Write(id)
+            if 'obj' in config['objects'][id]:
+                log.info('Writing id {0} to file {1}'.format(id, root_output_filename))
+                config['objects'][id]['obj'].Write(id)
+            else:
+                log.debug('Omitting id {0} since no root object found.'.format(id))
+                continue
 
         f.Close()
+
+
+class BuildTGraph(BaseModule):
+    def __init__(self):
+        super(BuildTGraph, self).__init__()
+        self.arg_group.add_argument('--build-tgraph', default=[], nargs='+', type='str2kvdict',
+                                    help='List of ids, which objects will be written out to a root file.')
+
+    def __call__(self, config):
+        if config['build_tgraph']:
+            print config['build_tgraph']
+            for id, input_ids in config['build_tgraph']:
+                if len(input_ids) == 1:
+                    # Basically only copies th TGraph
+                    new_graph =  ROOT.TGraphErrors(config['objects'][input_ids[0]]['obj'])
+                elif len(input_ids) == 2:
+                    # The both inputs define the minimum maximum and the center is set by the mean of both.
+                    tmp_tgraph1 = config['objects'][input_ids[0]]['obj']
+                    tmp_tgraph2 = config['objects'][input_ids[1]]['obj']
+                    if not (tmp_tgraph1.GetN() == tmp_tgraph2.GetN()):
+                        raise ValueError('The both objects must have the same number of points.')
+                    print type(tmp_tgraph1)
+                    new_graph = ROOT.TGraphAsymmErrors(tmp_tgraph1)
+                    for i in xrange(new_graph.GetN()):
+                        tmp_tgraph1x, tmp_tgraph1y = ROOT.Double(0), ROOT.Double(0)
+                        tmp_tgraph1.GetPoint(i, tmp_tgraph1x, tmp_tgraph1y)
+
+                        tmp_tgraph2x, tmp_tgraph2y = ROOT.Double(0), ROOT.Double(0)
+                        tmp_tgraph2.GetPoint(i, tmp_tgraph2x, tmp_tgraph2y)
+
+                        center = 0.5*(tmp_tgraph1y + tmp_tgraph2y)
+
+                        new_graph.SetPoint(i, tmp_tgraph1x, 0.5*(tmp_tgraph1y + tmp_tgraph2y))
+                        new_graph.SetPointEYlow(i, abs(tmp_tgraph1y - tmp_tgraph2y)/2.)
+                        new_graph.SetPointEYhigh(i, abs(tmp_tgraph1y - tmp_tgraph2y)/2.)
+
+                        new_graph.SetPointEXlow(i, tmp_tgraph1.GetErrorX(i))
+                        new_graph.SetPointEXhigh(i, tmp_tgraph1.GetErrorX(i))
+
+                elif len(input_ids) == 3:
+                    # The both inputs define the minimum maximum and the center is set by the mean of both.
+                    tmp_tgraph1 = config['objects'][input_ids[0]]['obj']
+                    tmp_tgraph2 = config['objects'][input_ids[1]]['obj']
+                    tmp_tgraph3 = config['objects'][input_ids[2]]['obj']
+                    if not (tmp_tgraph1.GetN() == tmp_tgraph2.GetN() == tmp_tgraph3.GetN()):
+                        raise ValueError('The input objects must have the same number of points.')
+
+                    new_graph = ROOT.TGraphAsymmErrors(tmp_tgraph1)
+
+                    for i in xrange(graph1.GetN()):
+                        tmp_tgraph1x, tmp_tgraph1y = ROOT.Double(0), ROOT.Double(0)
+                        tmp_tgraph1.GetPoint(i, tmp_tgraph1x, tmp_tgraph1y)
+
+                        tmp_tgraph2x, tmp_tgraph2y = ROOT.Double(0), ROOT.Double(0)
+                        tmp_tgraph2.GetPoint(i, tmp_tgraph2x, tmp_tgraph2y)
+
+                        tmp_tgraph3x, tmp_tgraph3y = ROOT.Double(0), ROOT.Double(0)
+                        tmp_tgraph3.GetPoint(i, tmp_tgraph3x, tmp_tgraph3y)
+
+                        new_graph.SetPoint(i, tmp_tgraph1X, tmp_tgraph1Y)
+                        new_graph.SetPointEYlow(i, tmp_tgraph1y - tmp_tgraph2y)
+                        new_graph.SetPointEYhigh(i, tmp_tgraph3y - tmp_tgraph1y)
+
+                        new_graph.SetPointEXlow(i, tmp_tgraph1.GetErrorX(i))
+                        new_graph.SetPointEXhigh(i, tmp_tgraph1.GetErrorX(i))
+
+                config['objects'].setdefault(id, {})['obj'] = new_graph
+
+
 
 
 def get_root_objects(input, option=None, **kwargs):
