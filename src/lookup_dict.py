0ece5532015139723615ef704a02e8cb6b0da28d
Georg Sieber <g.sieber@gmail.com>
2015-10-03 10:02:57 +0200

    update replacement dict.

--- src/lookup_dict.py
+++ src/lookup_dict.py
@@ -36,6 +36,7 @@ lookup_dict = {
     'ax_texts': {
         '_cms_': 'CMS?{"x": 0.0, "y":1.01, "va": "bottom", "ha" : "left"}',
         '_cmsp_': 'CMS Preliminary?{"x": 0.0, "y":1.01, "va": "bottom", "ha" : "left"}',
+        '_cmsp_': 'CMS Simulation?{"x": 0.0, "y":1.01, "va": "bottom", "ha" : "left"}',
         '_20fb_': '$19.7\\,\mathrm{fb}^{-1}$ (8TeV)?{"x": 1.0, "y":1.01, "va": "bottom", "ha" : "right"}',
         '_8tev_': '$\sqrt{s}$ = (8TeV)?{"x": 1.0, "y":1.01, "va": "bottom", "ha" : "right"}',
         '_yb0ys0_': '$0.0 \leq y_b < 1.0$  $0.0 \leq y^* < 1.0$',
