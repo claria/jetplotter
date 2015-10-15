ad1613e80dcbee54f8f149b0f52d7c39a2c810a3
Georg Sieber <g.sieber@gmail.com>
2015-10-15 16:08:35 +0200

    usable copy to web module.

--- modules/www_module.py
+++ modules/www_module.py
@@ -4,15 +4,42 @@ from modules.base_module import BaseModule
 
 log = logging.getLogger(__name__)
 
+import subprocess
+import os
+from datetime import date
+from datetime import datetime
 
-class CopyToWeb(BaseModule):
+def scp(src, remote, destination):
+    cmd = 'scp {0} {1}:{2}'.format(src, remote, destination)
+    log.debug('Executing command \'{0}\''.format(cmd))
+    rc = subprocess.call(cmd.split())
+    return rc
+
+def ssh(remote, cmd):
+    cmd = 'ssh {0} {1}'.format(remote, cmd)
+    log.debug('Executing command \'{0}\''.format(cmd))
+    rc = subprocess.call(cmd.split())
+    return rc
+
+
+class CopyToWebModule(BaseModule):
     def __init__(self):
-        super(CopyToWeb, self).__init__()
+        super(CopyToWebModule, self).__init__()
         self.arg_group.add_argument('--www', nargs='+', type='str2kvstr', action='setting', help='')
 
     def __call__(self, config):
-        # what should it do.
-        # create folder on target machine for current day.
-        # copy plot & json to folder
-        # run update_gallery script
-        pass
+        remote_host = 'sieber@ekplx77.physik.uni-karlsruhe.de' 
+        remote_basedir = 'public_html/private/plots/'
+
+        folder_name = date.today().isoformat()
+
+        output_file = config['output_path']
+        current_datetime = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+        remote_filename = '{0}_{1}{2}'.format(os.path.splitext(config['output_path'])[0], current_datetime, os.path.splitext(config['output_path'])[1])
+        log.debug('Try to create folder {0} on remote host.'.format(os.path.join(remote_basedir, folder_name)))
+        ssh(remote_host, 'mkdir -p {0}'.format(os.path.join(remote_basedir, folder_name)))
+        log.debug('Copying the output file.')
+        scp(os.path.join(config['output_prefix'], config['output_path']), remote_host, os.path.join(remote_basedir, folder_name, remote_filename))
+        log.debug('Calling the gallery preparation file.')
+        ssh(remote_host, '{0}/prep_gallery.py'.format(remote_basedir))
+
