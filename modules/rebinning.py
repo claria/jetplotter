7c4f7a7a0f1a41ddc72e62253849df35940af39d
Georg Sieber <g.sieber@gmail.com>
2015-11-29 22:39:30 +0100

    update rebinning.

--- modules/rebinning.py
+++ modules/rebinning.py
@@ -15,7 +15,7 @@ class ReBinning(BaseModule):
                                     help='Removes bins not within bin edges. Options are min and max.')
 
     def __call__(self, config):
-        print config.keys()
+        print config['data_lims']
         for id, kwargs in config['data_lims']:
             if id not in config['objects']:
                 raise ValueError('Requested id {} not found.'.format(id))
@@ -25,16 +25,15 @@ class ReBinning(BaseModule):
             min_val = kwargs.get('min', None)
             max_val = kwargs.get('max', None)
 
-            new_binedges = []
-            print min_val, max_val
-            for i in xrange(obj.GetNbinsX() +1):
-                if (min_val <= obj.GetBinLowEdge(i+1)) and (obj.GetBinLowEdge(i+2) <= max_val):
-                    new_binedges.append(obj.GetBinLowEdge(i+1))
+            config['objects'][id]['obj'] = rebin_histo(obj, min_val, max_val)
 
-            print new_binedges
+def rebin_histo(obj, min_val, max_val):
+    new_binedges = []
+    print min_val, max_val
+    for i in xrange(obj.GetNbinsX() +1):
+        if (min_val <= obj.GetBinLowEdge(i+1)) and (obj.GetBinLowEdge(i+2) <= max_val):
+            new_binedges.append(obj.GetBinLowEdge(i+1))
 
-            new_binedges = array.array("d", new_binedges)
-            new_obj = obj.Rebin(len(new_binedges) -1, '{0}_reb'.format(obj.GetName()), new_binedges)
-            config['objects'][id]['obj'] = new_obj
-
-
+    new_binedges = array.array("d", new_binedges)
+    new_obj = obj.Rebin(len(new_binedges) -1, '{0}_reb'.format(obj.GetName()), new_binedges)
+    return new_obj
