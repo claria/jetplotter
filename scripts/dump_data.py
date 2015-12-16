5f5b2f6c8e02ae7ce2d13fe2ee72091e957106ac
Georg Sieber <g.sieber@gmail.com>
2015-12-16 14:19:34 +0100

    respect sign of jec.

--- scripts/dump_data.py
+++ scripts/dump_data.py
@@ -69,6 +69,7 @@ def main():
             # data['{0}_dn'.format(jec_source)] = 1. - jec_dn.y/jec_default.y
             # data['{0}'.format(jec_source)] = np.abs((jec_up.y - jec_dn.y)/2.0)/jec_default.y * 100.
             data['{0}'.format(jec_source)] = np.maximum(np.abs(1. - jec_up.y/jec_default.y), np.abs(1. - jec_dn.y/jec_default.y)) * 100.
+            data['{0}'.format(jec_source)] = 0.5 *( (jec_up.y -jec_dn.y)/jec_default.y) * 100.
             # data['{0}'.format(jec_source)] = np.abs(1 - jec_up.y/jec_default.y) * 100.
 
         for k,v in data.iteritems():
