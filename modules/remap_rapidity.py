4d3016d5897240ca2f460eac96164021591d3a01
Georg Sieber <g.sieber@gmail.com>
2015-12-26 14:29:32 +0100

    remap rapidity module.

--- modules/remap_rapidity.py
+++ modules/remap_rapidity.py
@@ -34,3 +34,43 @@ class RemapRapidity(BaseModule):
 
 
             config['objects'][id]['obj'] = tg_remap
+
+class RemapRapidity2D(BaseModule):
+    """Calls the Project3D function."""
+
+    def __init__(self):
+        super(RemapRapidity2D, self).__init__()
+        self.arg_group.add_argument('--remap', nargs='+', type=str, default=[], help='')
+
+    def __call__(self, config):
+        for id in config['remap']:
+            obj = config['objects'][id]['obj']
+
+            nbins = obj.GetNbinsX() * obj.GetNbinsY()
+
+            tg_orig = ROOT.TGraph2DErrors(nbins)
+            ROOT.SetOwnership(tg_orig, 0)
+
+#             for i in xrange(tg_orig.GetN()):
+#                 origX, origY = ROOT.Double(0), ROOT.Double(0)
+#                 tg_orig.GetPoint(i, origX, origY)
+#
+#                 remapX = origX + origY
+#                 remapY = origX
+#                 tg_remap.SetPoint(i, remapX, remapY)
+#
+            for x in xrange(1, obj.GetNbinsX() + 1):
+                for y in xrange(1, obj.GetNbinsY() + 1):
+                    xval = obj.GetXaxis().GetBinCenter(x)
+                    yval = obj.GetYaxis().GetBinCenter(y) + obj.GetBinContent(x,y)
+                    zval = obj.GetYaxis().GetBinCenter(y)
+
+                    tg_orig.SetPoint(tg_orig.GetN(), xval, yval, zval)
+
+            # config['objects']['_tg_{0}'.format(id)] = {}
+
+            histo2d = tg_orig.GetHistogram().Clone('hallo')
+            ROOT.SetOwnership(histo2d, 0)
+            print histo2d
+            # config['objects']['_tg_{0}'.format(id)]['obj'] = tg_orig
+            config['objects'][id]['obj'] = tg_orig
