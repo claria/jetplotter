858d6c7caa41f7132204d801cd986d2288ef3bb6
Georg Sieber <g.sieber@gmail.com>
2016-02-07 17:41:17 +0100

    plot numbers in heatmap plot

--- util/plot_tools.py
+++ util/plot_tools.py
@@ -456,6 +456,11 @@ def plot_heatmap(obj, ax=None, z_log=False, z_lims=(None, None), cmap='viridis',
     z = obj.z
     if kwargs.get('mask_value') is not None:
         z = ma.masked_equal(z, kwargs.get('mask_value'))
+    if kwargs.get('plot_dataval') is not None:
+        for i, x_val in enumerate(obj.x):
+            for j, y_val in enumerate(obj.y):
+                c = '{0:.2g}'.format(z[i,j])
+                ax.text(x_val, y_val, c, va='center', ha='center')
 
     artist = ax.pcolormesh(x, y, z, linewidth=0., rasterized=True, cmap=cmap, norm=norm)
     artist.set_edgecolor('face')
